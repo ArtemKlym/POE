3dd01bd89493e51463dd591c80261cc2d56e07ee
Artem Klymenko <114606102+ArtemKlym@users.noreply.github.com>
2023-02-24 13:43:05 +0300

    optimized loop

--- src/Main.java
+++ src/Main.java
@@ -6,45 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static int x = 1, b = 1;
+    public static int b = 1, k = 1;
 
     public static void main(String[] args) {
-
-        String current = System.getProperty("user.dir");
-        System.out.println("Current working directory in Java : " + current);
-
         String path = System.getProperty("user.dir") + File.separator;
+        System.out.println("Current working directory in Java : " + path);
+
         File file[] = new File(path).listFiles();
-
-
-        for (int i = 0; i < file.length; i++) {
-            if(file[i].isDirectory())
-            {
-                System.out.println("->" + file[i].getName() + " (folder)"+x);
-                new Main().listfiles(file[i], "\t");
-                x++;
-            }
-            else
-            {
-                System.out.println("-> " + file[i].getName()+ " (file)"+i);
-            }
-        }
+        new Main().listfiles(file, "\t");
     }
 
-    public void listfiles(File f, String sprtr) {
-        int k = 1;
-        File file[] = f.listFiles();
+    public void listfiles(File[] f, String sprtr) {
+        File file[] = f;
         for (int i = 0; i < file.length; i++) {
             if (file[i].isDirectory()) {
                 System.out.println(sprtr + "-> " + file[i].getName() + " (folder)"+ k);
                 k++;
-                new Main().listfiles(file[i], sprtr+sprtr);
+                new Main().listfiles(file[i].listFiles(), sprtr + "\t");
             }
             else {
                 System.out.println(sprtr + file[i].getName()+" (file)"+b);
                 b++;
             }
-
         }
     }
 }
